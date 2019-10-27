8937ba00379221da60720a9570a591d770866bc7
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-10-27 02:18:36 -0400

    Updates

---
+++ precache-manifest.285c5a6612c17c61d29138f40c193c55.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e96e985e2950e2eada41a4342754d3",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "d0e4a3819bea08e7cdf9",
+    "url": "/react-memory-game/static/css/main.04d218cc.chunk.css"
+  },
+  {
+    "revision": "1dc670258ff6394e402b",
+    "url": "/react-memory-game/static/js/2.45002426.chunk.js"
+  },
+  {
+    "revision": "d0e4a3819bea08e7cdf9",
+    "url": "/react-memory-game/static/js/main.a8d4f602.chunk.js"
+  },
+  {
+    "revision": "e7cce8f96413fdbde7a2",
+    "url": "/react-memory-game/static/js/runtime-main.b1273a96.js"
+  },
+  {
+    "revision": "0d622a1f9e78c45b8926179f3959af2f",
+    "url": "/react-memory-game/static/media/AnaBray.0d622a1f.jpg"
+  },
+  {
+    "revision": "6a83c27497af17f68fbe30b084cc2673",
+    "url": "/react-memory-game/static/media/Cayde.6a83c274.jpeg"
+  },
+  {
+    "revision": "ee57994fec36464f772581d63ba3957c",
+    "url": "/react-memory-game/static/media/Hawthorne.ee57994f.jpeg"
+  },
+  {
+    "revision": "7c057b4de125ac0846c8862edc38e5a2",
+    "url": "/react-memory-game/static/media/Holiday.7c057b4d.jpeg"
+  },
+  {
+    "revision": "4a931fc753a1039d1705048af893c14f",
+    "url": "/react-memory-game/static/media/Ikora.4a931fc7.jpg"
+  },
+  {
+    "revision": "768bb2c7fe85b0fdc346567b219b0868",
+    "url": "/react-memory-game/static/media/Petra.768bb2c7.png"
+  },
+  {
+    "revision": "ff462506b6051648569da56ca5fc7a54",
+    "url": "/react-memory-game/static/media/Shaxx.ff462506.jpeg"
+  },
+  {
+    "revision": "f12fb57874d55333600917cabbfa542a",
+    "url": "/react-memory-game/static/media/Sloane.f12fb578.jpeg"
+  },
+  {
+    "revision": "f07d13f552f2069d9a288834a69e2f6c",
+    "url": "/react-memory-game/static/media/Spider.f07d13f5.png"
+  },
+  {
+    "revision": "e1af477766c78c4a6bb58009ec8ced17",
+    "url": "/react-memory-game/static/media/Tyra.e1af4777.jpeg"
+  },
+  {
+    "revision": "00425f95f7505cdae8d4d9787f177782",
+    "url": "/react-memory-game/static/media/Xur.00425f95.jpg"
+  },
+  {
+    "revision": "0fff6715d719a36a2581036b580ee40a",
+    "url": "/react-memory-game/static/media/Zavala.0fff6715.jpeg"
+  }
+]);
