a975aa3ef5bcf333107b0d1ad161ea715de9743c
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-03-08 03:20:46 -0400

    modified gitignore

---
+++ src/Assets/ImagesArray.js
@@ -0,0 +1,16 @@
+const imageNamesArray = [
+  'AnaBray.jpg',
+  'Cayde.jpeg',
+  'Hawthorne.jpeg',
+  'Holiday.jpeg',
+  'Ikora.jpg',
+  'Petra.png',
+  'Shaxx.jpeg',
+  'Sloane.jpeg',
+  'Spider.png',
+  'Tyra.jpeg',
+  'Xur.jpg',
+  'Zavala.jpeg'
+];
+
+export default imageNamesArray;
